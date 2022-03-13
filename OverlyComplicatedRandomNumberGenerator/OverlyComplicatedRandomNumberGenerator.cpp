005e980b889cb9f5c19616f18bcb8326ff69d776
Jason Burke <burkejasonj@icloud.com>
2022-03-12 22:06:48 -0800

    Add project files.

---
+++ OverlyComplicatedRandomNumberGenerator/OverlyComplicatedRandomNumberGenerator.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	printf("Hello OverlyComplicatedRandomNumberGenerator! Lets get some pseudocode in here!");
+
+	// To Generate a Random Number
+
+	//  
+	// INIT
+	//  
+	//  1. Clear OpMatrix
+	//  2. Get settings from file
+	//  3. Consult setting defaultNumOperations
+	//  4. Generate defaultNumOperations operations in OpMatrix
+	//  5. Enable OCRNG
+	//  
+	// GENERATE
+	//  
+	//  1. Consult OpMatrix and settings file
+	//  2. RNG normally
+	//  3. Apply OpMatrix to num
+	//  4. Add num to OpMatrix
+	//  5. Return num
+	//  
+	// TRIM
+	//  
+	//  1. Get OpMatrix length and minOpMatrixSize
+	//  2. Remove oldest entries of OpMatrix until minOpMatrixSize is met
+	//  3. Return num of entries trimmed
+	//  
+	// RANDOMIZE WEIGHTS
+	//  
+	//  1. Use GENERATE -OpMatrixFrozen to modify each OpMatrix weight without extending OpMatrix.
+	//  
+	//  
+	//  
+	//  
+	//  
+
+
+	return 0;
+}
